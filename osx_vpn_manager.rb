36339cdccf60c95f12dd7f3704aaea00af373d80
Stevenson Jean-Pierre <Stevenson.Jean-Pierre@sage.com>
2016-07-29 01:30:02 -0400

    Update osx_vpn_manager.rb

--- osx_vpn_manager.rb
+++ osx_vpn_manager.rb
@@ -2,7 +2,7 @@ class OsxVpnManager < Formula
   desc "Simple command-line vpn manager for OSX"
   homepage "https://github.com/Sjeanpierre/osx_vpn_manager"
   url "https://github.com/Sjeanpierre/osx_vpn_manager/releases/download/0.0.3/osx_vpn_manager-0.0.3.tar.gz"
-  sha256 "131e94ee650d9ee961479a14f6c6adcb09898467fd3beced544ea0b0a1cda2bc"
+  sha256 "a0a609729df134d8fef4ea1f18fd9851d28fdef4a4217e11cd8cf926c25a1382"
 
   depends_on "macosvpn"
 
